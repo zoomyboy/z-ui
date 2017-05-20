eb5d9632b3613ac07ea08137294e55f2cbc8095c
Philipp Lang <pille@zoomyboy.de>
2017-05-21 01:07:23 +0200

    Fixed: get-ref

--- w-next.vue
+++ w-next.vue
@@ -25,7 +25,7 @@
 			doNext: function() {
 				this.getRef(this.target).$emit('next');
 			},
-			getRef: require('../../methods/get-ref.js')
+			getRef: require('./get-ref.js')
 		},
 		mounted: function() {
 			var vm = this;
