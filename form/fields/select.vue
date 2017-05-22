f1d5800b676e1ad7009494c7b84c9219a4874376
Werbeagentur AWEOS <philipp@aweos.de>
2017-05-22 17:30:28 +0200

    Add select

--- form/fields/select.vue
+++ form/fields/select.vue
@@ -14,16 +14,64 @@
 
 <style lang="less">
 	@import "~select2/dist/css/select2.min.css";
+	@import "../../ui.less";
 
 	.vf-field-select-wrapper {
 		margin-bottom: 15px;
 		.select-label {
 			font-weight: bold;
 		}
+		& *:focus {
+			outline: none !important;
+			-webkit-appearance: none;
+		}
 	}
+
+	.select2-search.select2-search--dropdown {
+		&:focus, & > input:focus {
+			outline: none !important;
+			-webkit-appearance: none;
+		}
+	}
+
 	.select2.select2-container.select2-container--default {
 		display: block;
 		width: 100% !important;
+	}
+	span.selection > .select2-selection.select2-selection--single {
+		border-radius: 0;
+		border-color: @borders2;
+		height: 40px;
+		padding: 10px 15px;
+		.select2-selection__arrow {
+			height: 40px;
+			position: absolute;
+			top: 0px;
+			right: 0px;
+			width: 20px;
+		}
+		.select2-selection__rendered {
+			line-height: 20px;
+			padding: 0;
+		}
+	}
+	span.select2-dropdown {
+		border-color: #FE881D;
+		outline: 0;
+		box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(254, 136, 29, 0.6);
+	}
+
+	span.select2-container--focus, span.select2-container--open {
+		span.selection > .select2-selection.select2-selection--single  {
+			border-color: #FE881D;
+			outline: 0;
+			box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(254, 136, 29, 0.6);
+		}
+	}
+	
+	.select2-container--default .select2-results__option.select2-results__option--highlighted {
+		background-color: @bg-very-bright;
+		color: @link-bright;
 	}
 </style>
 
@@ -41,7 +89,7 @@
 				required: true,
 			},
 			value: {
-				default: function() {return null;},
+				default: null,
 			},
 			url: {
 				type: String,
@@ -65,15 +113,22 @@
 			value: function(newVal) {
 				if (typeof newVal == 'object') {
 					this.curValue = newVal.id;
+				} else {
+					this.curValue = newVal;
 				}
-
-				this.curValue = newVal;
+			},
+			curValue: function(newVal) {
+				var vm = this;
+				if (typeof newVal == 'number') {
+					var $select = $(vm.$refs.selectField);
+					$select.val(newVal).trigger('change');
+				}
 			}
 		},
 		data: function() {
 			return {
 				isField: true,
-				curValue: function() {return null;},
+				curValue: null,
 				error: false,
 				items: false
 			}
@@ -109,6 +164,10 @@
 					$select.append(`<option value="${item.id}">${item.title}</option>`);
 				});
 				$select.trigger('change');
+
+				$select.on('select2:select', function() {
+					vm.curValue = $(this).val();
+				});
 			});
 
 			this.$on('parseError', function(error) {
