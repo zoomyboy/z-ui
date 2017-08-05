7ed285f2a92e36633e06a5a895e01a741469dc78
Philipp Lang <pille@zoomyboy.de>
2017-08-06 01:25:09 +0200

    Fixed: Text for validationError

--- form/options/options.js
+++ form/options/options.js
@@ -6,7 +6,7 @@ module.exports = function() {
 		  sending: 'Sending form...',
 		  unauthorized: 'You\'re not allowed to do this!',
 		  notfound: 'This resource wasn\'t found',
-		  validationError: 'An error occured. Please check your input!'
+		  validationFailed: 'An error occured. Please check your input!'
 	  },
 	  fieldClass: 'form-control',
 	  ckeditor: true,
