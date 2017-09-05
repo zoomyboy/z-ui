6eaaf1b2d2abf91e3dbb3874cb3642122146d04a
Philipp Lang <philipp@aweos.de>
2017-09-05 15:48:33 +0200

    Add style for preview images

--- form/fields/filepreview.vue
+++ form/fields/filepreview.vue
@@ -1,25 +1,29 @@
 <template>
 	<div class="vf-field-child vf-field-file-child">
-		<img ref="preview" class="preview img-responsive">
-		<span title="Löschen" @click="del" class="delete-button fa fa-close"></span>
+		<div ref="preview" class="preview" :style="{'background-image': 'url('+img+')'}">
+			<span title="Löschen" @click="del" class="delete-button fa fa-close"></span>
+		</div>
 	</div>
 </template>
 
 <style lang="less">
 	.vf-field-child.vf-field-file-child {
-		position: relative;
-		.delete-button {
-			position: absolute;
-			top: 0;
-			right: 0;
-			margin-top: 14px;
-			margin-right: 14px;
-			font-size: 20px;
-			color: white;
-			cursor: pointer;
-		}
 		.preview {
 			margin-bottom: 8px;
+			position: relative;
+			width: 100%;
+			padding-top: 66%;
+			background-size: cover;
+			background-position: center center;
+			.delete-button {
+				position: absolute;
+				top: 14px;
+				right: 14px;
+				font-size: 20px;
+				color: white;
+				cursor: pointer;
+				width: auto;
+			}
 		}
 	}
 </style>
@@ -28,7 +32,8 @@
 	export default {
 		data: function() {
 			return  {
-				unique: ''
+				unique: '',
+				img: ''
 			};
 		},
 		props: {
@@ -46,12 +51,14 @@
 		},
 		methods: {
 			update: function() {
+				var vm = this;
+
 				var preview = this.$refs.preview;
 				var file = this.data.data;
 				var reader  = new FileReader();
 
 				reader.addEventListener("load", function () {
-					preview.src = reader.result;
+					vm.img = reader.result;
 				}, false);
 
 				if (file) {
--- form/fields/fileuploaded.vue
+++ form/fields/fileuploaded.vue
@@ -1,26 +1,29 @@
 <template>
 	<div class="vf-field-child vf-field-file-child">
-		<img :src="data.url" ref="preview" class="preview img-responsive">
-		<span title="Löschen" @click="del" class="delete-button fa fa-close"></span>
+		<div ref="preview" class="preview" :style="{'background-image': 'url('+data.url+')'}">
+			<span title="Löschen" @click="del" class="delete-button fa fa-close"></span>
+		</div>
 		<slot></slot>
 	</div>
 </template>
 
 <style lang="less">
 	.vf-field-child.vf-field-file-child {
-		position: relative;
-		.delete-button {
-			position: absolute;
-			top: 0;
-			right: 0;
-			margin-top: 14px;
-			margin-right: 14px;
-			font-size: 20px;
-			color: white;
-			cursor: pointer;
-		}
 		.preview {
-			margin-bottom: 8px;
+			position: relative;
+			width: 100%;
+			padding-top: 66%;
+			background-size: cover;
+			background-position: center center;
+			.delete-button {
+				position: absolute;
+				top: 14px;
+				right: 14px;
+				font-size: 20px;
+				color: white;
+				cursor: pointer;
+				width: auto;
+			}
 		}
 	}
 </style>
