2586ee5eddd18efcff8a29a3398f802a13bd7412
Philipp Lang <pille@zoomyboy.de>
2017-10-12 02:06:05 +0200

    Changed margin inside panel

--- panel.vue
+++ panel.vue
@@ -20,54 +20,58 @@
 	@import "~uiStyle";
 
 	.cp-wrap.cp-panel {
-	    border: 0;
-		border-radius: 3px;
-		box-shadow: 0 1px 1px rgba(0,0,0,0.2);
-		background-color: #fff;
-		.panel-heading {
-			border-top-left-radius: 2px;
-			border-top-right-radius: 2px;
-			height: 50px;
-			padding: 0;
-			color: #4d627b;
-    		border-bottom: 1px solid rgba(0,0,0,0.1);
-			padding: 0 20px;
-			display: flex;
-			flex-direction: row;
-			justify-content: space-between;
-			align-items: center;
-			.close-button {
-				font-size: 20px;
-				cursor: pointer;
-			}
-			.panel-title {
-				font-weight: 600;
-				font-size: 1.2em;
-				line-height: 50px;
-				white-space: nowrap;
-				overflow: hidden;
-				text-overflow: ellipsis;
-				&.smalltitle {
-					font-size: 1.0em;
+		margin-bottom: 0;
+		.panel {
+			border: 0;
+			border-radius: 3px;
+			box-shadow: 0 1px 1px rgba(0,0,0,0.2);
+			background-color: #fff;
+			.panel-heading {
+				border-top-left-radius: 2px;
+				border-top-right-radius: 2px;
+				height: 50px;
+				padding: 0;
+				color: #4d627b;
+				border-bottom: 1px solid rgba(0,0,0,0.1);
+				padding: 0 20px;
+				display: flex;
+				flex-direction: row;
+				justify-content: space-between;
+				align-items: center;
+				.close-button {
+					font-size: 20px;
+					cursor: pointer;
+				}
+				.panel-title {
+					font-weight: 600;
+					font-size: 1.2em;
+					line-height: 50px;
+					white-space: nowrap;
+					overflow: hidden;
+					text-overflow: ellipsis;
+					&.smalltitle {
+						font-size: 1.0em;
+					}
 				}
 			}
-		}
-		.panel-body {
-			.loading {
-				.double-bounce1, .double-bounce2 {
-					background: @primary;
+			.panel-body {
+				.loading {
+					.double-bounce1, .double-bounce2 {
+						background: @primary;
+					}
 				}
-			}
-			.table.table-striped {
-				&>tbody>tr:nth-child(2n+1) {
-    				background-color: #f5f7f8;
-				}
-				& > tbody td {
-					border-top-color: rgba(0,0,0,0.09);
-    				color: #8f9ea6;
-				}
-				&>thead {
-					color: #4d627b;
+				.table.table-striped {
+					margin-bottom: 0;
+					&>tbody>tr:nth-child(2n+1) {
+						background-color: #f5f7f8;
+					}
+					& > tbody td {
+						border-top-color: rgba(0,0,0,0.09);
+						color: #8f9ea6;
+					}
+					&>thead {
+						color: #4d627b;
+					}
 				}
 			}
 		}
