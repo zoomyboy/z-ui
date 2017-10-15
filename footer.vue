4d5aab82fd155d1d1abd10b52a471075ad0d1a74
Philipp Lang <pille@zoomyboy.de>
2017-10-15 22:44:10 +0200

    Add footer

---
+++ footer.vue
@@ -0,0 +1,40 @@
+<template>
+	<div :style="style" class="cp-wrap cp-footer">
+		
+	</div>
+</template>
+
+<style lang="less">
+	.cp-wrap.cp-z-ui-footer {
+		
+	}
+</style>
+
+<script>
+	export default {
+		props: {
+			height: {
+				default: 0,
+				required: true,
+				type: Number
+			}
+		},
+		data: function() {
+			return  {
+
+			};
+		},
+		computed: {
+			style: function() {
+				return {
+					"height": this.height+'px',
+					"min-height": this.height+'px',
+					'flex': '0 0 '+this.height+'px'
+				};
+			}
+		},
+		mounted: function() {
+
+		}
+	}
+</script>
